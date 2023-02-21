66515f452c4fbc5c09c862cd4f2081fe318f44ef
HungTruong4689 <hungphuc4689@gmail.com>
2023-02-21 20:05:15 +0100

    jumping on the cloud2

---
+++ easy/easySolution/jumpingOntheClouds2.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+    // Write your code here
+        int firstStep =0;
+        int countStep =0;
+        int position =0;
+        System.out.println(c.size());
+        while(firstStep  < c.size()-1 ){
+             System.out.println(firstStep);
+             
+            
+            if(firstStep +2 <=  c.size()-1 ){
+                if(c.get(firstStep +1) == 1  ){
+                firstStep = firstStep +2;
+                countStep++;
+                }
+                else if(c.get(firstStep +2) == 1 ){
+                    firstStep = firstStep +1;
+                    countStep++;
+                }
+                else if(c.get(firstStep +1) == 0 && c.get(firstStep +2) == 0 && firstStep +2 <=  c.size()-1){
+                firstStep = firstStep +2;
+                countStep++;
+                }
+            }else if(firstStep +2 ==  c.size()){
+                if(c.get(firstStep +1) == 0  ){
+                    firstStep = firstStep +1;
+                    countStep++;
+                }
+            }else{
+                break;
+            }
+            
+            
+            
+            
+        }
+        return countStep;
+    }
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
