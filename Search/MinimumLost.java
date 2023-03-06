be551c36374cc68ea443443f1b27a1214ac1dec2
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-06 17:50:20 +0100

    minimun loss treeset

---
+++ Search/MinimumLost.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'minimumLoss' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY price as parameter.
+     */
+
+    public static long minimumLoss(List<Long> price) {
+        // Write your code here
+        TreeSet<Long> list = new TreeSet<Long>();
+        long min = Long.MAX_VALUE;
+
+        for (int i = 0; i < price.size(); i++) {
+            Long header = list.higher(price.get(i));
+            System.out.println("header " + header);
+            if (header != null) {
+                min = Math.min(header - price.get(i), min);
+
+            }
+            list.add(price.get(i));
+        }
+        System.out.println(list);
+
+        return min;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] priceTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Long> price = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            long priceItem = Long.parseLong(priceTemp[i]);
+            price.add(priceItem);
+        }
+
+        long result = Result.minimumLoss(price);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
