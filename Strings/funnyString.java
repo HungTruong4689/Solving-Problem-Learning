bc2835f680dcfab5377e9d4fef228c58a4ed3fe5
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-07 15:31:00 +0100

    funny string

---
+++ Strings/funnyString.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+        // Write your code here
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+        List<Integer> list1 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<Integer>();
+        for (int i = 1; i < s.length(); i++) {
+            int val = Math.abs((+s.charAt(i)) - (+s.charAt(i - 1)));
+            list1.add(val);
+        }
+        for (int i = 1; i < rev.length(); i++) {
+            int val = Math.abs((+rev.charAt(i)) - (+rev.charAt(i - 1)));
+            list2.add(val);
+        }
+        int count = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) == list2.get(i)) {
+                count++;
+            }
+        }
+        if (count == list1.size()) {
+            return "Funny";
+        } else {
+            return "Not Funny";
+        }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
