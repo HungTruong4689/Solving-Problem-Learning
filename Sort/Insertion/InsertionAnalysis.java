d534ed99026609575cfe5a58710192baa66cca98
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-14 12:28:13 +0200

    insertion sort analysis

---
+++ Sort/Insertion/InsertionAnalysis.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static void swap(List<Integer> arr, int i, int j) {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+
+    public static int insertionSort(List<Integer> arr) {
+        // Write your code here
+        int count = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            int val = arr.get(i);
+            int j = i - 1;
+            while (j >= 0) {
+
+                if (arr.get(j) > arr.get(j + 1)) {
+                    swap(arr, j, j + 1);
+                    count++;
+                }
+                // arr.set(j+1,arr.get(j));
+                j--;
+            }
+            // arr.set(j+1,val);
+        }
+
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                int result = Result.insertionSort(arr);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
