61b3741bc9a7deca5cb7734639db16edff329b77
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-27 12:20:28 +0300

    making anagram

---
+++ Strings/easy/makingAnagram.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+    // Write your code here
+        int[] arr = new int[26];
+        for(int i =0; i < s1.length();i++){
+            int a = s1.charAt(i) - 'a';
+            arr[a]++;
+        }
+        for(int i =0; i < s2.length();i++){
+            int a = s2.charAt(i) - 'a';
+            arr[a]--;
+        }
+        int result =0;
+        for(Integer elm : arr){
+            result += Math.abs(elm);
+        }
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
